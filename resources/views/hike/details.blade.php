11e6143774102458d4124177de4d7a1075ef3886
CyrilleVerleye <160618056+CyrilleVerleye@users.noreply.github.com>
2024-05-29 16:57:26 +0200

    after tags done (not much)

--- resources/views/hike/details.blade.php
+++ resources/views/hike/details.blade.php
@@ -3,35 +3,34 @@
 
 <x-app-layout>
     <x-slot name="header">
-           {{ $hike->name }}
+        {{ $hike->name }}
     </x-slot>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-       <table>
-        <tr>
-            <th>name</th>
-            <th>location</th>
-            <th>distance</th>
-            <th>duration</th>
-            <th>elevation gain</th>
-            <th>description</th>
-            <th>created at:</th>
-            <th>updated at:</th>
-        </tr>
-        <tr>
-            <td>{{ $hike->name }}</td>
-            <td>{{ $hike->location }}</td>
-            <td>{{ $hike->distance}}</td>
-            <td>{{ $hike->duration }}</td>
-            <td>{{ $hike->elevation_gain }}</td>
-            <td>{{ $hike->description }}</td>
-            <td>{{ $hike->created_at }}</td>
-            <td>{{ $hike->updated_at }}</td>
-
-        </tr>
-    </table>
-</div>
-</div>
+            <table>
+                <tr class="text-left font-bold">
+                    <th class="px-4 py-3">name</th>
+                    <th>location</th>
+                    <th>distance</th>
+                    <th>duration</th>
+                    <th>elevation gain</th>
+                    <th>description</th>
+                    <th>created at:</th>
+                    <th>updated at:</th>
+                </tr>
+                <tr>
+                    <td class="border-t px-4 py-3">{{ $hike->name }}</td>
+                    <td>{{ $hike->location }}</td>
+                    <td>{{ $hike->distance}}</td>
+                    <td>{{ $hike->duration }}</td>
+                    <td>{{ $hike->elevation_gain }}</td>
+                    <td>{{ $hike->description }}</td>
+                    <td>{{ $hike->created_at }}</td>
+                    <td>{{ $hike->updated_at }}</td>
+                </tr>
+            </table>
+        </div>
+    </div>
 
 
 </x-app-layout>
