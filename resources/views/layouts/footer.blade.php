1d39eb4a083ba94cb77e220cdbe962fec56433a2
Crolweb <crolweb@gmail.com>
2024-08-05 09:25:14 +0200

    update footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="bg-white border-b border-gray-100">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <p>Project realised by <a href="www.crolweb.be">Crolweb</a> and <a href="https://github.com/CyrilleVerleye">Cyrille Verleye</a></p>
+        <p>Project realised by <a href="https://www.crolweb.be">Crolweb</a> and <a href="https://github.com/CyrilleVerleye">Cyrille Verleye</a></p>
     </div>
 
 </footer>
